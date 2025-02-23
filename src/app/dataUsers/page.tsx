fdf7d8063720364f1031e84ed1e5b3c275e2deea
willians henrique <willianshenrique94@gmail.com>
2025-02-23 13:07:34 -0300

    modified style

--- src/app/dataUsers/page.tsx
+++ src/app/dataUsers/page.tsx
@@ -10,6 +10,13 @@ export default function DataUsers() {
   const [statusFilter, setStatusFilter] = useState<'active' | 'inactive'>('active');
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [newUser, setNewUser] = useState({ name: "", email: "", password: "", role: "Corretor", avatar: "" });
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [selectedUsers, setSelectedUsers] = useState<string[]>([]); // Adicionando estado para armazenar os usuários selecionados
+  
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
 
   useEffect(() => {
     const fetchUserList = async () => {
@@ -90,125 +97,235 @@ export default function DataUsers() {
       console.error(error);
     }
   };
-  
+  // Funções para as ações do dropdown
+  const handleAction = async (action: string) => {
+    const token = getCookie("token");
+    if (!token) return;
+
+    try {
+      for (let userId of selectedUsers) {
+        if (action === "password") {
+          await handleChangePassword(userId);
+        } else if (action === "activate") {
+          await handleToggleStatus(userId);
+        } else if (action === "deactivate") {
+          await handleToggleStatus(userId);
+        } else if (action === "delete") {
+          await handleDeleteUser(userId);
+        }
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
-    <div className="bg-gray-200 flex flex-col items-center justify-center flex-1 p-4">
-      <h1 className="text-2xl font-bold text-black mb-4">Usuários</h1>
+    <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+      <div className="flex items-center justify-between flex-col flex-wrap md:flex-row space-y-4 md:space-y-0 pb-4 bg-white dark:bg-gray-900">
+        <div className="relative p-2">
+          <button
+            id="dropdownActionButton"
+            onClick={toggleDropdown}
+            className="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+            type="button"
+          >
+            <span className="sr-only">Action button</span>
+            Ação
+            <svg
+              className="w-2.5 h-2.5 ms-2.5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 10 6"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="m1 1 4 4 4-4"
+              />
+            </svg>
+          </button>
 
-      {/* Filtro de status */}
-      <div className="mb-4 flex gap-4">
-        <button
-          className={`px-4 py-2 rounded-md ${statusFilter === 'active' ? 'bg-teal-500 text-white' : 'bg-gray-300 text-black'}`}
-          onClick={() => setStatusFilter('active')}
-        >
-          Ativos
-        </button>
-        <button
-          className={`px-4 py-2 rounded-md ${statusFilter === 'inactive' ? 'bg-teal-500 text-white' : 'bg-gray-300 text-black'}`}
-          onClick={() => setStatusFilter('inactive')}
-        >
-          Inativos
-        </button>
-      </div>
-
-      {/* Tabela de listagem */}
-      {loading ? (
-        <p>Carregando...</p>
-      ) : (
-        <div className="overflow-y-auto max-h-96 w-full">
-          <table className="min-w-full table-auto border-collapse">
-            <thead>
-              <tr className="bg-gray-300 text-black">
-                <th className="border p-2">Nome</th>
-                <th className="border p-2">Email</th>
-                <th className="border p-2">Função</th>
-                <th className="border p-2">Status</th>
-                <th className="border p-2">Ações</th>
-              </tr>
-            </thead>
-            <tbody>
-            
-            {users.length > 0 ? (
-              users.map((user) => (
-                <tr key={user._id} className="text-black">
-                  <td className="border p-2">{user.name}</td>
-                  <td className="border p-2">{user.email}</td>
-                  <td className="border p-2">{user.role}</td>
-                  <td className="border p-2">{user.status}</td>
-                  <td className="border p-2">
-                    {/* Botões de ações */}
-                    <button
-                        onClick={() => handleChangePassword(user._id)}
-                        className="bg-blue-500 text-white px-4 py-2 rounded-md mr-2"
-                      >
-                        Senha Provisória
-                    </button>
-                    <button
-                      onClick={() => handleDeleteUser(user._id)}
-                      className="bg-red-500 text-white px-4 py-2 rounded-md mr-2"
-                    >
-                      Excluir
-                    </button>
-                    
-                    {user.status === 'active' ? (
-                      <button
-                        onClick={() => handleToggleStatus(user._id)}
-                        className="bg-yellow-500 text-white px-4 py-2 rounded-md mr-2"
-                      >
-                        Desativar
-                      </button>
-                    ) : (
-                      <button
-                        onClick={() => handleToggleStatus(user._id)}
-                        className="bg-green-500 text-white px-4 py-2 rounded-md mr-2"
-                      >
-                        Reativar
-                      </button>
-                    )}
-                    
-                  </td>
-                </tr>
-              ))
-            ) : (
-              <tr>
-                <td colSpan={5} className=" text-black border p-2 text-center">
-                  Nenhum usuário encontrado
-                </td>
-              </tr>
-            )}
-
-            </tbody>
-          </table>
+          {isDropdownOpen && (
+            <div
+              id="dropdownAction"
+              className="absolute z-10 bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700 dark:divide-gray-600"
+            >
+              <ul className="py-1 text-sm text-gray-700 dark:text-gray-200">
+                <li>
+                  <a
+                    href="#"
+                    onClick={() => handleAction("password")}
+                    className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    Senha Provisória
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    onClick={() => handleAction("activate")}
+                    className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    Ativar conta
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    onClick={() => handleAction("deactivate")}
+                    className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    Desativar conta
+                  </a>
+                </li>
+              </ul>
+              <div className="py-1">
+                <a
+                  href="#"
+                  onClick={() => handleAction("delete")}
+                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                >
+                  Deletar User
+                </a>
+              </div>
+            </div>
+          )}
         </div>
-      )}
 
-      {/* Botão para criar novo usuário */}
-      <button onClick={() => setIsModalOpen(true)} className="mt-4 bg-teal-500 text-white px-6 py-3 rounded-md hover:bg-teal-600">
-        Novo Usuário
-      </button>
+        {/* Filtro de status */}
+        <div className="mb-4 flex gap-4">
+          <button
+            className={`inline-flex items-center text-gray-500 border border-gray-300 focus:outline-none focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-3 py-1.5 dark:text-gray-400 dark:border-gray-600 dark:focus:ring-gray-700 ${statusFilter === 'active' ? 'bg-gray-200 text-white' : 'bg-white dark:bg-gray-800 dark:hover:bg-gray-700 text-black dark:text-gray-400 dark:border-gray-600 dark:hover:border-gray-600'}`}
+            onClick={() => setStatusFilter('active')}
+          >
+            Ativos
+          </button>
+          <button
+            className={`inline-flex items-center text-gray-500 border border-gray-300 focus:outline-none focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-3 py-1.5 dark:text-gray-400 dark:border-gray-600 dark:focus:ring-gray-700 ${statusFilter === 'inactive' ? 'bg-gray-200 text-white' : 'bg-white dark:bg-gray-800 dark:hover:bg-gray-700 text-black dark:text-gray-400 dark:border-gray-600 dark:hover:border-gray-600'}`}
+            onClick={() => setStatusFilter('inactive')}
+          >
+            Inativos
+          </button>
+        </div>
 
-      {isModalOpen && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-          <div className="bg-white p-6 rounded-lg shadow-lg">
-            <h2 className="text-black text-xl font-bold mb-4">Criar Novo Usuário</h2>
-            <input type="text" placeholder="Nome" className=" text-black border p-2 w-full mb-2" value={newUser.name} onChange={(e) => setNewUser({ ...newUser, name: e.target.value })} />
-            <input type="email" placeholder="Email" className="text-black border p-2 w-full mb-2" value={newUser.email} onChange={(e) => setNewUser({ ...newUser, email: e.target.value })} />
-            <input type="password" placeholder="Senha" className="text-black border p-2 w-full mb-2" value={newUser.password} onChange={(e) => setNewUser({ ...newUser, password: e.target.value })} />
-            <select className="text-black border p-2 w-full mb-2" value={newUser.role} onChange={(e) => setNewUser({ ...newUser, role: e.target.value })}>
-              <option value="Corretor">Corretor</option>
-              <option value="Administrativo">Administrativo</option>
-              <option value="Financeiro">Financeiro</option>
-              <option value="SuperUsuario">SuperUsuario</option>
-            </select>
-            <input type="text" placeholder="Link do Avatar" className="text-black border p-2 w-full mb-2" value={newUser.avatar} onChange={(e) => setNewUser({ ...newUser, avatar: e.target.value })} />
-            <div className="flex justify-end mt-4">
-              <button onClick={() => setIsModalOpen(false)} className="bg-gray-400 text-white px-4 py-2 rounded-md mr-2">Cancelar</button>
-              <button onClick={handleCreateUser} className="bg-teal-500 text-white px-4 py-2 rounded-md">Criar</button>
+        {/* Tabela de listagem */}
+        {loading ? (
+          <p>Carregando...</p>
+        ) : (
+          <div className="p-2 pb-4 overflow-y-auto max-h-96 w-full">
+            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+              <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                  <th scope="col" className="p-4">
+                    <div className="flex items-center">
+                      <input
+                        id="checkbox-all-search"
+                        type="checkbox"
+                        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                        onChange={(e) => {
+                          // Lógica para selecionar todos os usuários
+                          setSelectedUsers(e.target.checked ? users.map(user => user._id) : []);
+                        }}
+                      />
+                      <label htmlFor="checkbox-all-search" className="sr-only">checkbox</label>
+                    </div>
+                  </th>
+                  <th scope="col" className="px-6 py-3">Name</th>
+                  <th scope="col" className="px-6 py-3">Função</th>
+                  <th scope="col" className="px-6 py-3">Status</th>
+                  <th scope="col" className="px-6 py-3">Contato</th>
+                </tr>
+              </thead>
+              <tbody>
+                {users.filter(user => user.status === statusFilter).length === 0 ? (
+                  <tr>
+                    <td colSpan={5} className="text-center p-4">
+                      Não há usuários {statusFilter === 'active' ? 'ativos' : 'inativos'} no momento.
+                    </td>
+                  </tr>
+                ) : (
+                  users.map((user) => (
+                  <tr
+                    key={user._id}
+                    className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+                  >
+                    <td className="p-4">
+                      <div className="flex items-center">
+                        <input
+                          id={`checkbox-user-${user._id}`}
+                          type="checkbox"
+                          className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                          onChange={(e) => {
+                            const selectedUserIds = e.target.checked
+                              ? [...selectedUsers, user._id]
+                              : selectedUsers.filter(id => id !== user._id);
+                            setSelectedUsers(selectedUserIds);
+                          }}
+                        />
+                        <label
+                          htmlFor={`checkbox-user-${user._id}`}
+                          className="sr-only"
+                        >
+                          checkbox
+                        </label>
+                      </div>
+                    </td>
+                    <td
+  scope="row"
+  className="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white"
+>
+  <img
+    className="w-10 h-10 rounded-full"
+    src="/default-avatar.jpg" // Usar um avatar padrão se não houver
+    alt={`${user.name} avatar`}
+  />
+  <div className="ps-3">
+    <div className="text-base font-semibold">{user.name}</div>
+    <div className="font-normal text-gray-500">{user.email}</div>
+  </div>
+</td>
+
+                    <td className="px-6 py-4">{user.role}</td>
+                    <td className="px-6 py-4">{user.status}</td>
+                    <td className="px-6 py-4">email</td>
+                  </tr>
+                  
+                )))}
+              </tbody>
+            </table>
+          </div>
+        )}
+
+        {/* Botão para criar novo usuário */}
+        <button onClick={() => setIsModalOpen(true)} className="mt-4 inline-flex items-center text-gray-500 border border-gray-300 focus:outline-none focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-6 py-3 bg-gray-200 dark:text-gray-400 dark:border-gray-600 dark:focus:ring-gray-700">
+          Novo Usuário
+        </button>
+
+        {isModalOpen && (
+          <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+            <div className="bg-white p-6 rounded-lg shadow-lg">
+              <h2 className="text-black text-xl font-bold mb-4">Criar Novo Usuário</h2>
+              <input type="text" placeholder="Nome" className=" text-black border p-2 w-full mb-2" value={newUser.name} onChange={(e) => setNewUser({ ...newUser, name: e.target.value })} />
+              <input type="email" placeholder="Email" className="text-black border p-2 w-full mb-2" value={newUser.email} onChange={(e) => setNewUser({ ...newUser, email: e.target.value })} />
+              <input type="password" placeholder="Senha" className="text-black border p-2 w-full mb-2" value={newUser.password} onChange={(e) => setNewUser({ ...newUser, password: e.target.value })} />
+              <select className="text-black border p-2 w-full mb-2" value={newUser.role} onChange={(e) => setNewUser({ ...newUser, role: e.target.value })}>
+                <option value="Corretor">Corretor</option>
+                <option value="Administrativo">Administrativo</option>
+                <option value="Financeiro">Financeiro</option>
+                <option value="SuperUsuario">SuperUsuario</option>
+              </select>
+              <input type="text" placeholder="Link do Avatar" className="text-black border p-2 w-full mb-2" value={newUser.avatar} onChange={(e) => setNewUser({ ...newUser, avatar: e.target.value })} />
+              <div className="flex justify-end mt-4">
+                <button onClick={() => setIsModalOpen(false)} className="bg-gray-400 text-white px-4 py-2 rounded-md mr-2">Cancelar</button>
+                <button onClick={handleCreateUser} className="bg-teal-500 text-white px-4 py-2 rounded-md">Criar</button>
+              </div>
             </div>
           </div>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 }
